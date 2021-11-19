63425c99dbbee8e44a516fbca8f224b66ded88d0
Saberrytwo <90337853+Saberrytwo@users.noreply.github.com>
2021-11-18 17:37:29 -0700

    Adding models and admin registration

--- clothingpages/models.py
+++ clothingpages/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+
+class items(models.Model):
+    firstName = models.CharField(max_length=30)
+    lastName = models.CharField(max_length=30)
+    clothingType = models.CharField(max_length=30)
+    materialType = models.CharField(max_length=30)
+    brandName = models.CharField(max_length=30)
+    ownerRating = models.DecimalField(2, 1)
+    memory = models.CharField(max_length=300)
+
+    def __str__(self):
+        return(self.firstName + " " + self.lastName + ": " + self.clothingType)
